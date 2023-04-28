eea1e8e86490f0bb30d7925ef8a0bb24f6f3eca3
Michaela <botka@email.cz>
2023-04-28 11:07:23 +0200

    smazány přebytečné fotky

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ window.addEventListener("scroll", function(){
         const currentScroll = window.pageYOffset;
 
         if (currentScroll <= 0) {
-                body.classList.remove("scroll-up")
+                body.classList.remove("scroll-up");
         } else if (currentScroll >  lastScroll && !body.classList.contains("scroll-down")){
-                body.classList.remove("scroll-up")
-                body.classList.add("scroll-down")
+                body.classList.remove("scroll-up");
+                body.classList.add("scroll-down");
         } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) { 
-                body.classList.remove("scroll-down")
-                body.classList.add("scroll-up")
+                body.classList.remove("scroll-down");
+                body.classList.add("scroll-up");
         };
 
         lastScroll = currentScroll;
@@ -31,16 +31,16 @@ let navigation = document.querySelector(".navigation");
 
 
 menu.addEventListener("click", function(){
-        menu.style.display = "none"
-        close.style.display = "block"
-        navigation.classList.toggle("activ-menu")
-})
+        menu.style.display = "none";
+        close.style.display = "block";
+        navigation.classList.toggle("activ-menu");
+});
 
 close.addEventListener("click", function(){
-                close.style.display = "none"
-                menu.style.display = "block"
-                navigation.classList.toggle("activ-menu")
-})
+                close.style.display = "none";
+                menu.style.display = "block";
+                navigation.classList.toggle("activ-menu");
+});
 
 
 
@@ -51,22 +51,22 @@ close.addEventListener("click", function(){
 
 // TENISOVÉ SOUSTŘEDĚNÍ - více info po rozkliknutí
 
-let btnSoustredeni = document.querySelector(".btn-soustredeni")
-let soustredeni3 = document.querySelector(".soustredeni3")
+let btnSoustredeni = document.querySelector(".btn-soustredeni");
+let soustredeni3 = document.querySelector(".soustredeni3");
 
 
 btnSoustredeni.addEventListener("click", function(){
        if (soustredeni3.style.display=="none"){
-        soustredeni3.style.display = "block"
-        btnSoustredeni.innerHTML="Méně informací &#9650"
+        soustredeni3.style.display = "block";
+        btnSoustredeni.innerHTML="Méně informací &#9650";
         } else if (soustredeni3.style.display=="block"){
-        soustredeni3.style.display = "none"
-        btnSoustredeni.innerHTML="Více informací &#9660"
+        soustredeni3.style.display = "none";
+        btnSoustredeni.innerHTML="Více informací &#9660";
         } else {
-        soustredeni3.style.display = "block" 
-        btnSoustredeni.innerHTML="Méně informací &#9650"  
-        }  
-})
+        soustredeni3.style.display = "block" ;
+        btnSoustredeni.innerHTML="Méně informací &#9650" ; 
+        }
+});
 
 
 
@@ -75,26 +75,26 @@ btnSoustredeni.addEventListener("click", function(){
 
 // TENISOVÝ OBCHOD - kliknutí na btn open nebo Telephone
 
-let btnOpen = document.querySelector(".btn-open")
-let btnPhone = document.querySelector(".btn-phone")
+let btnOpen = document.querySelector(".btn-open");
+let btnPhone = document.querySelector(".btn-phone");
 
 btnPhone.addEventListener("click", function(){
-       btnPhone.classList.toggle("active")
-        btnOpen.classList.toggle("active")
+       btnPhone.classList.toggle("active");
+        btnOpen.classList.toggle("active");
 });
 
 btnOpen.addEventListener("click", function(){
-        btnPhone.classList.toggle("active")
-         btnOpen.classList.toggle("active")
+        btnPhone.classList.toggle("active");
+         btnOpen.classList.toggle("active");
  });
 
 
 
 
 // //  kliknutí na úspěch-foto, zvětší se a naopak
-let uspechFoto = document.querySelectorAll(".uspech-foto")
+let uspechFoto = document.querySelectorAll(".uspech-foto");
 
 uspechFoto.forEach(function(oneUspech){
         oneUspech.addEventListener("click", function(){
-                oneUspech.classList.toggle("zoom")
-        })})
+                oneUspech.classList.toggle("zoom");
+        })});
